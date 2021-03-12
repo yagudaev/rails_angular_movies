037d218064d89b9c3797fe73bfcd64c64b675416
Michael Yagudaev <myagudaev@usertesting.com>
2021-03-12 15:26:22 -0800

    Add database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+GROUP_SIZE = 10_000
+movies_json = JSON.parse(open("db/movies.json").read)
+
+ActiveRecord::Base.transaction do
+  group_num = 0
+  movies_json.in_groups_of(GROUP_SIZE) do |movie_group|
+    group_num += 1
+    data = movie_group.map do |movie|
+      return nil unless movie
+
+      {
+        title: movie["title"],
+        year: movie["year"],
+        image_url: movie["image"],
+        color: movie["color"],
+        score: movie["score"],
+        rating: movie["rating"],
+
+        # needed by insert_all since it doesn't use life-cycle hooks
+        created_at: Time.now,
+        updated_at: Time.now
+      }
+    end.compact
+    Movie.insert_all(data)
+
+    puts "Created #{ActiveSupport::NumberHelper.number_to_delimited(group_num * GROUP_SIZE)} movie records...."
+  end
+end
+
+puts "🏁 Successfully finished creating records"
